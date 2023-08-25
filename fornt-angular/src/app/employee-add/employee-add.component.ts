1516acd659c5267eef17b02c64fac87fad0fad2f
Rafael Santos Neves <r4fael.neves2004@gmail.com>
2023-08-25 15:08:42 -0300

    Desenvolvimento da validação dos campos usado o validator do angular

--- fornt-angular/src/app/employee-add/employee-add.component.ts
+++ fornt-angular/src/app/employee-add/employee-add.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-add',
   templateUrl: './employee-add.component.html',
   styleUrls: ['./employee-add.component.css']
 })
-export class EmployeeAddComponent {
+
+export class EmployeeAddComponent{
+
+  employeeFrom!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.createForm();
+  }
+
+  createForm(){
+    this.employeeFrom = this.formBuilder.group({
+      // n aceita campos vazios
+      employeeName: ['', Validators.required], 
+      jobRole: ['', Validators.required], 
+      salary: ['', Validators.required], 
+      birth: ['', Validators.required], 
+      employeeRegistration: ['', Validators.required], 
+    })
+  }
+
 
 }
